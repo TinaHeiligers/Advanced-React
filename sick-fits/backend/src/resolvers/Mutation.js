5ffa5890b8c0c1d8f742002f62889bcf2fab9311
tinaheiligers <tina.heiligers@gmail.com>
2018-11-06 09:48:19 -0700

    reformats slightly

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -4,14 +4,8 @@ const Mutations = {
   async createItem(parent, args, ctx, info) {
     //TODO Check if they are logged in
 
-    const item = await ctx.db.mutation.createItem(
-      {
-        data: {
-          ...args
-        }
-      },
-      info
-    );
+    const item = await ctx.db.mutation.createItem({
+        data: { ...args } }, info);
     return item;
   },
   // createDog(parent, args, ctx, info) {
