f6719760740567c18ae0e3a6d6976975c5f8e9e4
TinaHeiligers <tina.heiligers@gmail.com>
2018-11-07 16:06:22 -0700

    Removes console log from creating an item mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -3,7 +3,6 @@
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     //TODO Check if they are logged in
-
     const item = await ctx.db.mutation.createItem({
         data: { ...args } }, info);
     return item;
