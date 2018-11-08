9576fde7c375da682e516448e00e603942b882a0
TinaHeiligers <tina.heiligers@gmail.com>
2018-11-08 08:48:06 -0700

    Finishes pagination

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -6,6 +6,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
 };
 
 module.exports = Query;
