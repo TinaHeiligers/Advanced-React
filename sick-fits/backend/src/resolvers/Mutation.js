7686cbf8bd7bd7e2fee96891c637d3113b6511c0
TinaHeiligers <tina.heiligers@gmail.com>
2018-11-09 16:35:22 -0700

    Adds sign in form and a new wrapping 'shaddow' html element to wrap adjacent elements

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -55,7 +55,28 @@ const Mutations = {
       maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
     });
     return user;
-  }
+  },
+  async signin(parent, { email, password }, ctx, info) {
+    // 1. check if there is a user with the email provided
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    // 2. check if their password is provided
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid password!')
+    }
+    // 3. generate the JWT
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // 4. set the cookie with the tooken
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365,
+    });
+    // 5. return the user
+    return user;
+  },
 };
 
 module.exports = Mutations;
