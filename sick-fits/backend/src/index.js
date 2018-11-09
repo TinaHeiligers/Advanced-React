ad3cc7840c307fb75c78fff20f357095ad8c0d71
TinaHeiligers <tina.heiligers@gmail.com>
2018-11-09 14:27:25 -0700

    Logs users in

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,6 +1,7 @@
 // Entry point of the application
 const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
+const jwt = require('jsonwebtoken');
 const createServer = require('./createServer');
 const db = require('./db');
 
@@ -9,6 +10,15 @@ const server = createServer();
 // use express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
 // TODO use express middleware to populate current user
+// Decode the jwt so we can get the userId on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET)
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -7,6 +7,15 @@ const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    //check if there is a current UserId
+    if(!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user({
+      where: { id: ctx.request.userId },
+    }, info);
+  },
 };
 
 module.exports = Query;
