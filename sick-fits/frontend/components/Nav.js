5cafda3e9001d0756b860c8e5264d7e49ce4e5d7
TinaHeiligers <tina.heiligers@gmail.com>
2018-11-11 15:52:53 -0700

    Uses local state with mutations and queries to toggle the cart state

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,17 +1,19 @@
-import Link from 'next/link';
-import NavStyles from './styles/NavStyles';
-import User from './User';
-import Signout from './Signout';
+import Link from "next/link";
+import { Mutation } from "react-apollo";
+import NavStyles from "./styles/NavStyles";
+import User from "./User";
+import Signout from "./Signout";
+import { TOGGLE_CART_MUTATION } from "./Cart";
 
 const Nav = () => (
-    <User>
-      {({ data: { me } }) => (
-        <NavStyles>
-          <Link href="/items">
-            <a>Shop</a>
-          </Link>
-          { me && (
-            < >
+  <User>
+    {({ data: { me } }) => (
+      <NavStyles>
+        <Link href="/items">
+          <a>Shop</a>
+        </Link>
+        {me && (
+          <>
             <Link href="/sell">
               <a>Sell</a>
             </Link>
@@ -22,15 +24,18 @@ const Nav = () => (
               <a>Account</a>
             </Link>
             <Signout />
-            < />
-            )}
-          {(!me && (
-            <Link href="/signup">
-              <a>Sign In</a>
-            </Link>
-            ))}
-        </NavStyles>
-      )}
-    </User>
-)
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+            </Mutation>
+          </>
+        )}
+        {!me && (
+          <Link href="/signup">
+            <a>Sign In</a>
+          </Link>
+        )}
+      </NavStyles>
+    )}
+  </User>
+);
 export default Nav;
