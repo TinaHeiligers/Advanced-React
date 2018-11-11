7ab23356b31306f27ca6e094df1544f20c0146d4
TinaHeiligers <tina.heiligers@gmail.com>
2018-11-10 17:18:34 -0700

    Adds page for rendering out current user permissions and html elements to change these

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -2,6 +2,7 @@
 // We write our custom queries in here with middleware to handle authenticatio
 // If the query is exactly the same both on Prisma and on Yoga, we can forward the Yoga query directly to prisma
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
   items: forwardTo('db'),
@@ -16,6 +17,16 @@ const Query = {
       where: { id: ctx.request.userId },
     }, info);
   },
+  async users(parent, args, ctx, info) {
+    // 1. check if they are logged in
+    if(!ctx.request.userId) {
+      throw new Error('You must be logged in')
+    }
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    // 2. check if the user has the permissions to query all the users
+    return ctx.db.query.users({}, info);
+    // 3. query all the users
+  }
 };
 
 module.exports = Query;
