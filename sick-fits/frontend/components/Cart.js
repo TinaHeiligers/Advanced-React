26485c0dd44d91800fc8302864ea5210fc7bda72
Christiane (Tina) Heiligers <tina@herbalflow.co.za>
2018-11-12 16:28:57 -0800

    Cleans up the render prop situation in Cart.js using {adopt} from 'react-adopt' (#17)

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Query, Mutation } from "react-apollo";
 import gql from "graphql-tag";
+import { adopt } from "react-adopt";
 import User from "./User";
 import CartStyles from "./styles/CartStyles";
 import Supreme from "./styles/Supreme";
@@ -21,49 +22,48 @@ const TOGGLE_CART_MUTATION = gql`
     toggleCart @client
   }
 `;
-
+// we pass the render function in as a child to get rid of the proptypes error.
+const Composed = adopt({
+  user: ({ render }) => <User>{render}</User>,
+  toggleCart: ({ render }) => (
+    <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>
+  ),
+  localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>
+});
 const Cart = props => (
-  <User>
-    {({ data: { me } }) => {
+  <Composed>
+    {({ user, toggleCart, localState }) => {
+      const me = user.data.me;
       if (!me) return null;
       return (
-        <Mutation mutation={TOGGLE_CART_MUTATION}>
-          {toggleCart => (
-            <Query query={LOCAL_STATE_QUERY}>
-              {({ data }) => (
-                <CartStyles open={data.cartOpen}>
-                  <header>
-                    <CloseButton onClick={toggleCart} title="close">
-                      &times;
-                    </CloseButton>
-                    <Supreme>
-                      {`${me.name}${me.name.slice(-1) === "s" ? "'" : "'s"}`}{" "}
-                      Cart
-                    </Supreme>
-                    <p>
-                      You have {me.cart.length} Item
-                      {me.cart.length === 1 ? "" : "s"} in your cart.
-                    </p>
-                  </header>
-                  <ul>
-                    {me.cart.map(cartItem => (
-                      <CartItem key={cartItem.id} cartItem={cartItem}>
-                        {cartItem.id}
-                      </CartItem>
-                    ))}
-                  </ul>
-                  <footer>
-                    <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-                    <SickButton>Checkout</SickButton>
-                  </footer>
-                </CartStyles>
-              )}
-            </Query>
-          )}
-        </Mutation>
+        <CartStyles open={localState.data.cartOpen}>
+          <header>
+            <CloseButton onClick={toggleCart} title="close">
+              &times;
+            </CloseButton>
+            <Supreme>
+              {`${me.name}${me.name.slice(-1) === "s" ? "'" : "'s"}`} Cart
+            </Supreme>
+            <p>
+              You have {me.cart.length} Item
+              {me.cart.length === 1 ? "" : "s"} in your cart.
+            </p>
+          </header>
+          <ul>
+            {me.cart.map(cartItem => (
+              <CartItem key={cartItem.id} cartItem={cartItem}>
+                {cartItem.id}
+              </CartItem>
+            ))}
+          </ul>
+          <footer>
+            <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+            <SickButton>Checkout</SickButton>
+          </footer>
+        </CartStyles>
       );
     }}
-  </User>
+  </Composed>
 );
 
 export default Cart;
