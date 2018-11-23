c33a8542654e041cfeaf858868ac078379cb468a
Christiane (Tina) Heiligers <tina@herbalflow.co.za>
2018-11-23 15:44:21 -0700

    Displays Single Order on it's own page using a new component that maps over items and returns an OrderItem functional component for each (#23)

---
+++ sick-fits/frontend/components/Order.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Query } from "react-apollo";
+import { format } from "date-fns";
+import Head from "next/head";
+import gql from "graphql-tag";
+import formatMoney from "../lib/formatMoney";
+import Error from "./ErrorMessage";
+import OrderStyles from "./styles/OrderStyles";
+import OrderItem from "./OrderItem";
+
+const SINGLE_ORDER_QUERY = gql`
+  query SINGLE_ORDER_QUERY($id: ID!) {
+    order(id: $id) {
+      id
+      charge
+      total
+      createdAt
+      user {
+        id
+      }
+      items {
+        id
+        title
+        description
+        price
+        image
+        quantity
+      }
+    }
+  }
+`;
+
+class Order extends Component {
+  static propTypes = {
+    id: PropTypes.string.isRequired
+  };
+  render() {
+    return (
+      <Query query={SINGLE_ORDER_QUERY} variables={{ id: this.props.id }}>
+        {({ data, error, loading }) => {
+          if (error) return <Error error={error} />;
+          if (loading) return <p>Loading...</p>;
+          const order = data.order;
+          return (
+            <OrderStyles>
+              <Head>
+                <title>Sick Fits - Order {order.id}</title>
+              </Head>
+              <p>
+                <span>Order ID:</span>
+                <span>{this.props.id}</span>
+              </p>
+              <p>
+                <span>charge</span>
+                <span>{order.charge}</span>
+              </p>
+              <p>
+                <span>Date</span>
+                <span>{format(order.createdAt, "dd MMMM yyyy h:mm a")}</span>
+              </p>
+              <p>
+                <span>Order Total</span>
+                <span>{formatMoney(order.total)}</span>
+              </p>
+              <p>
+                <span>Item{order.items.length != 1 ? "s" : ""} Count</span>
+                <span>{order.items.length}</span>
+              </p>
+              <div className="items">
+                {order.items.map(item => (
+                  <OrderItem item={item} key={item.id} />
+                ))}
+              </div>
+            </OrderStyles>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+export default Order;
