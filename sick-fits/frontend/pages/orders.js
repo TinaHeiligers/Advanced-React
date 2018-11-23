0d33a11e480e3aad7fb68b7003231478ff86d95f
Christiane (Tina) Heiligers <tina@herbalflow.co.za>
2018-11-23 16:49:33 -0700

    53 displaying all user orders (#24)
    * Adds ordersList page and component, creates query for graphql
    
    * Adds an orders list page

---
+++ sick-fits/frontend/pages/orders.js
@@ -0,0 +1,11 @@
+import PleaseSignIn from "../components/PleaseSignIn";
+import OrdersList from "../components/OrdersList";
+
+const OrdersPage = props => (
+  <div>
+    <PleaseSignIn>
+      <OrdersList />
+    </PleaseSignIn>
+  </div>
+);
+export default OrdersPage;
