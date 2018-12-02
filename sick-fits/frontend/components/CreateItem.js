f10fc4cd38d5852a2579e62c30533785715cfdfc
Christiane (Tina) Heiligers <tina@herbalflow.co.za>
2018-12-02 16:55:06 -0700

    62 test request reset mutation (#32)
    * Adds test for requestReset, mocking out the mutation. Also updates .eslintrc
    
    * Adds a test that mocks fetch API and interacts with the createItem form, peaking into state to check updates and also checks routing

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { Mutation } from 'react-apollo';
-import gql from 'graphql-tag';
-import Router from 'next/router';
-import Form from './styles/Form';
-import formatMoney from '../lib/formatMoney';
-import Error from './ErrorMessage';
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import Router from "next/router";
+import Form from "./styles/Form";
+import formatMoney from "../lib/formatMoney";
+import Error from "./ErrorMessage";
 
 const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
@@ -28,102 +28,113 @@ const CREATE_ITEM_MUTATION = gql`
 
 class CreateItem extends Component {
   state = {
-    title: '',
-    description: '',
-    image: '',
-    largeImage: '',
-    price: 0,
+    title: "",
+    description: "",
+    image: "",
+    largeImage: "",
+    price: 0
   };
   handleChange = e => {
     const { name, type, value } = e.target;
-    const val = type === 'number' ? parseFloat(value) : value;
+    const val = type === "number" ? parseFloat(value) : value;
     this.setState({ [name]: val });
-  }
+  };
   uploadFile = async e => {
     const files = e.target.files;
     const data = new FormData();
-    data.append('file', files[0]);
-    data.append('upload_preset', 'sickfits')
-    const res = await fetch
-      ('https://api.cloudinary.com/v1_1/tinaheiligers/image/upload', {
-        method: 'POST',
+    data.append("file", files[0]);
+    data.append("upload_preset", "sickfits");
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/tinaheiligers/image/upload",
+      {
+        method: "POST",
         body: data
-      });
-      const file = await res.json();
-      this.setState({
-        image: file.secure_url,
-        largeImage: file.eager[0].secure_url
-      });
-  }
+      }
+    );
+    const file = await res.json();
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
         {(createItem, { loading, error }) => (
-        <Form
-          onSubmit={ async e => {
-            e.preventDefault();
-            const res = await createItem();
-            Router.push({
-              pathname: '/item',
-              query: { id: res.data.createItem.id},
-            });
-          }}
-        >
-          <Error error={error} />
-          <fieldset disabled={loading} aria-busy={loading}>
-            <label htmlFor="file">
-              Image
-              <input
-                type="file"
-                id="file"
-                name="file"
-                placeholder="Upload an image"
-                required
-                onChange={this.uploadFile}
-              />
-              { this.state.image && <img src={this.state.image} width="200"alt="Upload Preview"/>}
-            </label>
-            <label htmlFor="title">
-              Title
-              <input
-                type="text"
-                id="title"
-                name="title"
-                placeholder="Title"
-                required
-                value={this.state.title}
-                onChange={this.handleChange}
-              />
-            </label>
-            <label htmlFor="price">
-              Price
-              <input
-                type="number"
-                id="price"
-                name="price"
-                placeholder="Price"
-                required
-                value={this.state.price}
-                onChange={this.handleChange} />
-            </label>
-            <label htmlFor="description">
-              Description
-              <textarea
-                id="description"
-                name="description"
-                placeholder="Enter A Description"
-                required
-                value={this.state.description}
-                onChange={this.handleChange} />
-            </label>
-            <button type="submit">Submit</button>
-          </fieldset>
-          <h2>Sell an item.</h2>
-        </Form>
+          <Form
+            data-test="form"
+            onSubmit={async e => {
+              e.preventDefault();
+              const res = await createItem();
+              Router.push({
+                pathname: "/item",
+                query: { id: res.data.createItem.id }
+              });
+            }}
+          >
+            <Error error={error} />
+            <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && (
+                  <img
+                    src={this.state.image}
+                    width="200"
+                    alt="Upload Preview"
+                  />
+                )}
+              </label>
+              <label htmlFor="title">
+                Title
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
+                  required
+                  value={this.state.title}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="price">
+                Price
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
+                  required
+                  value={this.state.price}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label htmlFor="description">
+                Description
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="Enter A Description"
+                  required
+                  value={this.state.description}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <button type="submit">Submit</button>
+            </fieldset>
+            <h2>Sell an item.</h2>
+          </Form>
         )}
       </Mutation>
     );
   }
 }
 export default CreateItem;
-export { CREATE_ITEM_MUTATION }
+export { CREATE_ITEM_MUTATION };
--- sick-fits/frontend/components/RequestReset.js
+++ sick-fits/frontend/components/RequestReset.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Mutation } from 'react-apollo';
-import gql from 'graphql-tag';
-import Form from './styles/Form';
-import Error from './ErrorMessage';
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import Form from "./styles/Form";
+import Error from "./ErrorMessage";
 
 const REQUEST_RESET_MUTATION = gql`
   mutation REQUEST_RESET_MUTATION($email: String!) {
@@ -14,27 +14,30 @@ const REQUEST_RESET_MUTATION = gql`
 
 class RequestReset extends Component {
   state = {
-    email: '',
+    email: ""
   };
   saveToState = e => {
     this.setState({ [e.target.name]: e.target.value });
-  }
+  };
   render() {
     return (
-      <Mutation
-        mutation={REQUEST_RESET_MUTATION}
-        variables={this.state}
-        >
+      <Mutation mutation={REQUEST_RESET_MUTATION} variables={this.state}>
         {(reset, { error, loading, called }) => (
-          <Form method="post" onSubmit={ async e => {
-            e.preventDefault();
-            await reset();
-            this.setState({ email: '' });
-          }}>
+          <Form
+            method="post"
+            data-test="form"
+            onSubmit={async e => {
+              e.preventDefault();
+              await reset();
+              this.setState({ email: "" });
+            }}
+          >
             <fieldset disabled={loading} aria-busy={loading}>
               <h2>Request a Password Reset</h2>
               <Error error={error} />
-              { !error && !loading && called && <p>Success! Check your email for a reset link!</p>}
+              {!error && !loading && called && (
+                <p>Success! Check your email for a reset link!</p>
+              )}
               <label htmlFor="email">
                 Email
                 <input
@@ -42,15 +45,16 @@ class RequestReset extends Component {
                   name="email"
                   placeholder="email"
                   value={this.state.email}
-                  onChange={this.saveToState}/>
+                  onChange={this.saveToState}
+                />
               </label>
               <button type="submit">Request Reset!</button>
             </fieldset>
           </Form>
-        )
-      }
+        )}
       </Mutation>
     );
   }
 }
 export default RequestReset;
+export { REQUEST_RESET_MUTATION };
