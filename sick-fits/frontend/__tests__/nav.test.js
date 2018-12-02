e70c004d5c3ba470d2809498e2be64b8c60f9bce
TinaHeiligers <tina.heiligers@gmail.com>
2018-12-02 12:14:37 -0700

    Completes nav testing with snapshots of children

--- sick-fits/frontend/__tests__/nav.test.js
+++ sick-fits/frontend/__tests__/nav.test.js
@@ -4,7 +4,7 @@ import wait from "waait";
 import Nav from "../components/Nav";
 import { CURRENT_USER_QUERY } from "../components/User";
 import { MockedProvider } from "react-apollo/test-utils";
-import { fakeUser } from "../lib/testUtils";
+import { fakeUser, fakeCartItem } from "../lib/testUtils";
 // create mocks outside of the tests
 export const notSignedInMocks = [
   {
@@ -20,6 +20,20 @@ export const signedInMocks = [
   }
 ];
 
+export const signedInMocksWithCartItems = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: {
+      data: {
+        me: {
+          ...fakeUser(),
+          cart: [fakeCartItem(), fakeCartItem(), fakeCartItem()]
+        }
+      }
+    }
+  }
+];
+
 describe("<Nav />", () => {
   it("renders out a minimal nav when a user is not signed in", async () => {
     const wrapper = mount(
@@ -32,6 +46,35 @@ describe("<Nav />", () => {
     // we'll use a snapshot to test this.
     const nav = wrapper.find('[data-test="nav"]');
     expect(toJSON(nav)).toMatchSnapshot();
+  });
+  it("renders a full nav when a user is signed in", async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocks}>
+        <Nav />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    // Use a snapshot to test that the full nav is rendered
+    const nav = wrapper.find("ul[data-test='nav']");
+    expect(nav.children().length).toBe(6);
+    expect(nav.text()).toContain("Sign Out");
+    // expect(toJSON(nav)).toMatchSnapshot();
+  });
+  it("renders the amount of items in the cart", async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocksWithCartItems}>
+        <Nav />
+      </MockedProvider>
+    );
+    await wait();
+    wrapper.update();
+    // Use a snapshot to test that the full nav is rendered
     // console.log(wrapper.debug());
+    const nav = wrapper.find("[data-test='nav']");
+    // console.log(nav.debug());
+    const count = nav.find("div.count");
+    // console.log("count", count.debug());
+    expect(toJSON(count)).toMatchSnapshot();
   });
 });
